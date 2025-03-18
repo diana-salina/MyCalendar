f2a5486d5a51e66f0f1a813cbdcba7469dd15dac
diana-salina <dianasalina@yandex.ru>
2025-03-18 20:11:32 +0700

    authorization window started

--- app/src/main/java/ru/nsu/mycalendar/MainActivity.kt
+++ app/src/main/java/ru/nsu/mycalendar/MainActivity.kt
@@ -1,6 +1,8 @@
 package ru.nsu.mycalendar
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+//        val loginText: TextView = findViewById(R.id.loginText)
+//        val passwordText: TextView = findViewById(R.id.passwordText)
+        val login: EditText = findViewById(R.id.loginForm)
+        val password: EditText = findViewById(R.id.passwordForm)
+        val loginButton: Button = findViewById(R.id.button)
+        loginButton.setOnClickListener {
+            if (login.text.isEmpty() || password.text.isEmpty()) {
+                println("login unsuccessful")
+            } else {
+                println("login successful")
+            }
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
